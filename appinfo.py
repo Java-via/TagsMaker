7105a48a4a76f09baa5d5379fae2ec639316140e
zhangjie <zhangjie@reyun.com>
2016-08-02 16:50:14 +0800

    add manager model

--- appinfo.py
+++ appinfo.py
@@ -1,2 +1,46 @@
 # _*_ coding: utf-8 _*_
 
+import pymysql
+import logging
+from flask import jsonify
+
+# ----server----
+SDB_HOST = "101.200.174.172"
+SDB_DB = "data_apps"
+SDB_USER = "dba_apps"
+SDB_PWD = "mimadba_apps"
+SDB_CHARSET = "utf8"
+
+# ----local----
+DB_HOST = "127.0.0.1"
+DB_DB = "my_db"
+DB_USER = "root"
+DB_PWD = "123"
+DB_CHARSET = "utf8"
+
+
+def save():
+    try:
+        conn = pymysql.connect(host=SDB_HOST, user=SDB_USER, password=SDB_PWD, db=SDB_DB, charset=SDB_CHARSET)
+        cur = conn.cursor()
+        sql = "SELECT a_pkgname, a_name, a_description, a_classify, a_defaulttags, a_url, a_softgame " \
+              "FROM t_apps_basic_united WHERE DATE(a_getdate) = '2016-07-30';"
+        cur.execute(sql)
+        conn.commit()
+        apps = cur.fetchall()
+        conn.close()
+        conn_l = pymysql.connect(host=DB_HOST, user=DB_USER, password=DB_PWD, db=DB_DB, charset=DB_CHARSET)
+        cur_l = conn_l.cursor()
+        sql_l = "INSERT INTO t_tags (t_pkgname, t_name, t_description, t_classify, " \
+                "t_defaulttags, t_url, t_softgame)  VALUES (%s, %s, %s, %s, %s, %s, %s);"
+        for app in apps:
+            cur_l.execute(sql_l, (app[0], app[1], app[2], app[3], app[4], app[5], app[6]))
+            conn_l.commit()
+        return
+
+    except Exception as e:
+        logging.error(Exception, ":", e)
+        return
+
+if __name__ == "__main__":
+    save()
--- index.py
+++ index.py
@@ -6,6 +6,22 @@ from flask import Flask, render_template
 from flask import request, jsonify
 
 logging.basicConfig(level=logging.DEBUG)
+
+# ----server----
+SDB_HOST = "101.200.174.172"
+SDB_DB = "data_apps"
+SDB_USER = "dba_apps"
+SDB_PWD = "mimadba_apps"
+SDB_CHARSET = "utf8"
+
+# ----local----
+DB_HOST = "127.0.0.1"
+DB_DB = "my_db"
+DB_APPDB = "app_db"
+DB_USER = "root"
+DB_PWD = "123"
+DB_CHARSET = "utf8"
+
 app = Flask(__name__)
 
 
@@ -50,7 +66,7 @@ def tagsgame():
 
 def userlogin(useremail, userpwd):
     try:
-        conn = pymysql.connect(host="127.0.0.1", user="root", password="123", db="my_db", charset="utf8")
+        conn = pymysql.connect(host=DB_HOST, user=DB_USER, password=DB_PWD, db=DB_DB, charset=DB_CHARSET)
         cur = conn.cursor()
         cur.execute("SELECT u_id FROM t_users WHERE u_email = %s AND u_pwd = %s", (useremail, userpwd))
         conn.commit()
@@ -67,7 +83,7 @@ def userlogin(useremail, userpwd):
 @app.route("/gameinfo", methods=["POST", "GET"])
 def gameinfo():
     try:
-        conn = pymysql.connect(host="localhost", user="root", password="123", db="app_db", charset="utf8")
+        conn = pymysql.connect(host=DB_HOST, user=DB_USER, password=DB_PWD, db=DB_APPDB, charset=DB_CHARSET)
         cur = conn.cursor()
         logging.debug("====1====")
         sql = "SELECT a_pkgname, a_name, a_description, a_defaulttags, a_classify, a_url FROM t_apps_basic_united " \
@@ -84,6 +100,45 @@ def gameinfo():
         logging.error(Exception, ":", e)
         return jsonify({"msg": "no data"})
 
+
+@app.route("/gametags", methods=["POST", "GET"])
+def gametags():
+    if request.method == "GET":
+        pkgname = request.args.get("pkgname")
+        sexual = request.args.get("sexual")
+        age = request.args.get("age")
+        marital = request.args.get("marital")
+        logging.debug("pkgname = %s, sexual = %s, age = %s, marital = %s", pkgname, sexual, age, marital)
+        try:
+            conn = pymysql.connect(host=DB_HOST, user=DB_USER, password=DB_PWD, db=DB_DB, charset=DB_CHARSET)
+            cur = conn.cursor()
+            sql = "INSERT INTO t_tags (t_sexual, t_age, t_marital, t_degree) VALUES (%s, %s, %s, %s) " \
+                  "WHERE t_pkgname = %s"
+            cur.execute(sql, (sexual, age, marital, "学士", pkgname))
+            conn.commit()
+            return jsonify({"msg": "success"})
+        except Exception as e:
+            logging.error(Exception, ":", e)
+            return jsonify({"msg": "sql error"})
+    else:
+        logging.error("trans gametags with wrong request method: %s", request.method)
+        return jsonify({"msg": "fail"})
+
+
+def savegametags():
+    return
+
+
+@app.route("/userinfo", methods=["POST", "GET"])
+def userinfo():
+    if request.method == "GET":
+        conn = pymysql.connect(host=DB_DB, user=DB_USER, password=DB_PWD, charset=DB_CHARSET)
+        cur = conn.cursor()
+        sql = "SELECT u_email, u_name, u_pwd, u_manager FROM t_users"
+        cur.execute(sql)
+        user = cur.fetchall()
+        if len(user) < 0:
+            logging.error("selece users error")
 
 if __name__ == "__main__":
     app.run(debug=True)
