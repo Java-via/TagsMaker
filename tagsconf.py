a2469e5248f490c394713a42b7ea946f57c82907
zhangjie <zhangjie@reyun.com>
2016-08-11 11:18:41 +0800

    add tagsconf

--- tagsconf.py
+++ tagsconf.py
@@ -1,1 +1,17 @@
 # _*_ coding: utf-8 _*_
+
+config_tags_soft = {
+    {"gender": "性别偏好"}: {"male": "男", "female": "女", "all": "无偏向"},
+    {"marital": "婚姻偏好"}: {"unmarried": "未婚", "married": "已婚", "all": "无偏向"},
+    {"age": "年龄偏好"}: {"teen": "18岁以下", "youth": "18-25岁", "old_youth": "26-35岁",
+                      "earth_mid": "36-45岁", "over_mid": "45岁以上"},
+    {"degree": "学历偏好"}: {"middle": "小学/初中", "high": "高中/中专/大专", "university": "大学及以上", "all": "无偏向"},
+}
+
+config_tags_game = {
+    {"gender": "性别偏好"}: {"male": "男", "female": "女", "all": "无偏向"},
+    {"marital": "婚姻偏好"}: {"unmarried": "未婚", "married": "已婚", "all": "无偏向"},
+    {"age": "年龄偏好"}: {"teen": "18岁以下", "youth": "18-25岁", "old_youth": "26-35岁",
+                      "earth_mid": "36-45岁", "over_mid": "45岁以上"},
+    {"degree": "学历偏好"}: {"middle": "小学/初中", "high": "高中/中专/大专", "university": "大学及以上", "all": "无偏向"},
+}
